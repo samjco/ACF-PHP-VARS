253068016a0547825100f3f1005001a58ac4ea10
Sam C <samjco@gmail.com>
2018-09-27 02:12:36 -0500

    Updated for PRO
    Fixes on the wpdb prefixes, sql queries, and detection on if ACF exists in wp-content/plugins folder OR in any custom user created plugins/themes.

--- inc/acf-spv-options.php
+++ inc/acf-spv-options.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @author 		Sam 
+ * @version     1.2
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
 //Simple function to check if partial strings are present in bigger ones. We'll use this later.
 function p_Strings($thi_s, $tha_t){
     $pos = strpos($thi_s,$tha_t);
@@ -12,50 +21,89 @@ function p_Strings($thi_s, $tha_t){
 
 add_action( 'admin_menu', 'add_acf_spv_setting_page' ); 
 
+
+
 function add_acf_spv_setting_page() {
-	add_menu_page('options-general.php', 'ACF PHP VARS', 'edit_theme_options', 'acf_spv_show_variables', 'acf_spv_show_variables', ACFSPVURL.'/css/icons/sm-acf-logo.png');
+	//Show as it's own Menu
+	//add_menu_page('options-general.php', 'ACF Theme PHP', 'edit_theme_options', 'acf_spv_show_variables', 'acf_spv_show_variables', ACFSPVURL.'/css/icons/sm-acf-logo.png');
+
+	if(is_plugin_active('advanced-custom-fields-pro/acf.php') || ( class_exists('acf_pro') )):
+		//Show under Custom Fields Menu
+	    add_submenu_page('edit.php?post_type=acf-field-group', 'ACF Theme PHP', 'ACF Theme PHP', 'manage_options', 'acf_spv_show_variables', 'acf_spv_show_variables');
+	else:
+	    add_submenu_page('edit.php?post_type=acf', 'ACF Theme PHP', 'ACF Theme PHP', 'manage_options', 'acf_spv_show_variables', 'acf_spv_show_variables');
+	endif;
+
 }
 
-function acf_spv_show_variables(){
+function acf_spv_show_variables($fgvar){
 	global $wpdb;
 	global $post;
 
+		//if(is_plugin_active('advanced-custom-fields-pro/acf.php')): // OLD
+		if( !class_exists('acf_pro') ):
+			$ACF_PRO_active = false;	
+		else:
+			$ACF_PRO_active = true;
+		endif; 
+
+
 	//Grab ACF Field Group Object
-	$acf_field_group_VARS = $wpdb->get_results('SELECT * FROM  wp_postmeta WHERE meta_key =  "rule"');
-	//Creates the table. The ID and Class make the sorting work. ?>
-	<h2>ACF Show PHP Variables | Template Usage</h2>
+
+	//Creates the table. The ID and Class make the sorting work. 
+		$acf_sql1="SELECT * FROM  {$wpdb->prefix}postmeta WHERE meta_key =  'rule'";
+		// $acf_pro_sql1="SELECT * FROM {$wpdb->prefix}postmeta WHERE meta_key = '_edit_last'"; //OLD
+		$acf_pro_sql2="SELECT * FROM {$wpdb->prefix}posts INNER JOIN {$wpdb->prefix}postmeta ON {$wpdb->prefix}posts.ID = {$wpdb->prefix}postmeta.post_id WHERE 1=1 AND {$wpdb->prefix}posts.post_type = 'acf-field-group' AND {$wpdb->prefix}posts.post_status = 'publish' AND {$wpdb->prefix}postmeta.meta_key = '_edit_last' ORDER BY {$wpdb->prefix}posts.menu_order, {$wpdb->prefix}posts.post_date DESC";
+	  
+	    if(!$ACF_PRO_active):
+	    //ACF
+	    $acf_field_group_VARS = $wpdb->get_results($acf_sql1);
+		else:
+		//ACF PRO
+	    $acf_field_group_VARS = $wpdb->get_results($acf_pro_sql2);
+		endif;
+
+
+
+		?>
+		
+
+	<h2 class="acfpv-title">ACF Show PHP Variables | Template Usage</h2>
+
 	<p class="description">(Copy and Paste into your template file.)</p>
 
 	<?php if($acf_field_group_VARS[0] != ''){ ?>
 		<table id="acf-table" class="tablesorterx wp-list-table widefat users">
 			<thead>
-				<tr> 
+				<tr class="tr-tablehead"> 
 					
 
-					<th><strong>Variables for ACF function:</strong>
-						<?php 
-						$getf = " get_field(&#39;variable_name&#39;);";
-						$thef = " the_field(&#39;variable_name&#39;);";
-						$getfinst = " - This function gets the string of a value and allows you to store in a variable.";
-						$thefinst = " - This function immediately displays values and cannot be store as a string.";
-                        echo "<span style='padding:2px 6px;background:#eee;' class='code'>";
-						echo "<span class='change-function' data-name1='" . $thef . "' data-name2='".$getf."'>" . $getf . "</span>"; 
-						echo "</span>";
-						echo "<span class='description'>";
-						echo "<span class='change-function' data-name1='" . $thefinst . "' data-name2='".$getfinst."'>" . $getfinst . "</span>"; 
-						echo "</span>";
-						?>
-						<br><div style="margin-top:8px;padding:6px 0px; border: 1px solid #ddd;background:#eee;">
+				<th><span class="vartitle"><strong>Variables for ACF function:</strong></span>
+					<?php 
+					$getf = " get_field(&#39;variable_name&#39;);";
+					$thef = " the_field(&#39;variable_name&#39;);";
+					$getfinst = " - This function gets the string of a value and allows you to store in a variable.";
+					$thefinst = " - This function immediately displays values and cannot be store as a string.";
+					echo "<div class='vardesc'";
+                    echo "<span style='padding:2px 6px;background:#eee;' class='code'>";
+					echo "<span class='change-function' data-name1='" . $thef . "' data-name2='".$getf."'>" . $getf . "</span>"; 
+					echo "</span>";
+					echo "<span class='description'>";
+					echo "<span class='change-function' data-name1='" . $thefinst . "' data-name2='".$getfinst."'>" . $getfinst . "</span>"; 
+					echo "</span>";
+					echo "</div>";
+					?>
+					<br><div class="toggle-container" style="margin-top:8px;padding:6px 0px; border: 1px solid #ddd;background:#eee;">
 
-						<!-- Button to Copy Paste ALL -->
-					    <button style="margin-left:8px;" class="btn" data-clipboard-action="copy" data-clipboard-target="#acf-table tbody">Copy All to Clipboard</button>
+					<!-- Button to Copy Paste ALL -->
+				    <button style="margin-left:8px;" class="btn ctc" data-clipboard-action="copy" data-clipboard-target="#acf-table tbody">Copy All to Clipboard</button>
 
-						<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;font-weight:bold;" class="code">SHOW/TOGGLE:</label> 
-						<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code"><input type="checkbox" name="vauChange" value="changefunction" id="toggle-function-id"> Use Function: the_field</label>
-						<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code"><input type="checkbox" name="vauChange" value="changekeys" id="toggle-key-id"> Use Field Keys</label>
-						<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code"><input type="checkbox" name="vauChange" value="showtype" id="toggle-type-id"> Field Type</label>
-						<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code"><input type="checkbox" name="vauChange" value="showinstructions" id="toggle-instructions-id"> Instructions</label>
-						<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code"><input type="checkbox" name="vauChange" value="showexample" id="toggle-example-id"> Example Code</label>
+					<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;font-weight:bold;" class="code toggle">SHOW/TOGGLE:</label> 
+					<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code toggle-function-id"><input type="checkbox" name="vauChange" value="changefunction" id="toggle-function-id"> Function:the_field</label>
+					<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code toggle-key-id"><input type="checkbox" name="vauChange" value="changekeys" id="toggle-key-id"> Field Keys</label>
+					<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code toggle-type-id"><input type="checkbox" name="vauChange" value="showtype" id="toggle-type-id"> Field Type</label>
+					<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code toggle-instructions-id"><input type="checkbox" name="vauChange" value="showinstructions" id="toggle-instructions-id"> Instructions</label>
+					<label style="border-right:1px solid #ddd;padding:4px 16px 4px 8px;" class="code toggle-example-id"><input type="checkbox" name="vauChange" value="showexample" id="toggle-example-id"> Example Code</label>
 
 					</div>
 					</th>
@@ -63,51 +111,81 @@ function acf_spv_show_variables(){
 				
 				</tr>
 			</thead>
-		    <?php		
+		    <?php	
 			foreach ($acf_field_group_VARS as $acf_field_group_VAR) {
+		    					//var_dump($acf_field_group_VAR);
+		    					//die();
+
 				
 				//Grab the id the of ACF Field group
 				$acf_field_group_var_ID = $acf_field_group_VAR->post_id;
-				
 				//Get more from the object by using the get_post() function. Ultimately, we're trying to get
 
 				//the name of the field group
 				$acf_field_group = get_post($acf_field_group_var_ID);
 				$acf_field_group_TITLE = $acf_field_group->post_title;
 
-				//Gets the custom field keys from the groups ID
-				$acf_field_group_KEYS = get_post_custom_keys($acf_field_group_var_ID);
-				//Create an array that will hold our individual field keys				 
-				$acf_field_group_keys_ARRAY = array();			
-				//Check to see if field_ exists in the array item and if it does, push the field into the new array we just created
-				foreach ($acf_field_group_KEYS as $acf_field_group_KEY) {
-					if(p_Strings($acf_field_group_KEY, "field_")){
-						array_push($acf_field_group_keys_ARRAY, $acf_field_group_KEY);
+			    if(!$ACF_PRO_active):
+					//Gets the custom field keys from the groups ID
+					$acf_field_group_KEYS = get_post_custom_keys($acf_field_group_var_ID);
+
+					//Create an array that will hold our individual field keys				 
+					$acf_field_group_keys_ARRAY = array();			
+					//Check to see if field_ exists in the array item and if it does, push the field into the new array we just created
+					foreach ($acf_field_group_KEYS as $acf_field_group_KEY) {
+						if(p_Strings($acf_field_group_KEY, "field_")){
+							array_push($acf_field_group_keys_ARRAY, $acf_field_group_KEY);
+						}
+
 					}
 
-				}
+				else:
+					//If ACF PRO
+		            $acf_field_field_VARS = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}posts WHERE post_parent = '".$acf_field_group_var_ID."' ");
+		        	$acf_field_group_keys_ARRAY = $acf_field_field_VARS;
+				endif;
+		
 
-				//now, we can grab the field objects with our newly created field key
+				//Show in Metabox on Field Group screen
+				if($fgvar):
+					if ($fgvar === sanitize_title($acf_field_group_TITLE)):
+						echo "<style type='text/css'>div#div-".$fgvar."{padding: 0 !important;}.acfpv-title{padding:0 !important;display:none;}tr.tr-".$fgvar."{background: white !important;}#acf-table td{padding:0 !important;}table#acf-table{padding: 0 !important;display: inline-block;width: 100%;overflow: auto;}</style>";
 
-					echo "<tr id=''><td><div id='div-".sanitize_title($acf_field_group_TITLE)."' class='code' style='padding:25px;'>";
+						$hide ="";
+					else:
+						$hide="hide";
+	 				endif;
+	 			
+ 				endif;
+
+					echo "<tr class='tr-".sanitize_title($acf_field_group_TITLE)." ".$hide."'><td><div id='div-".sanitize_title($acf_field_group_TITLE)."' class='code' style='padding:25px;'>";
 					if($count = 1): 
 						$count++; 
-					    echo '<div style="display:block;float:right;"><span style="opacity:0;">&lt;!---</span><button style="" class="btn noselect" data-clipboard-action="copy" data-clipboard-target="#div-'.sanitize_title($acf_field_group_TITLE).'"> Copy Code </button><span style="opacity:0;">--&gt;</span></div>';
-
-					    echo "<pre>&lt;?php<br>/*<br>";
-					    echo "<br>*************************************<br>";
-					    echo "ACF FIELD GROUP:<br>".$acf_field_group_TITLE . "<br>";
-					    echo "*************************************<br>";
-					    echo "Made by ACF PHP VARS plugin<br>*************************************<br>*/</pre>"; 
+					    echo '<div class="noselect" style="display:block;text-align:left;"><span style="opacity:0;">&lt;!---</span><br><a class="button btn noselect" data-clipboard-action="copy" data-clipboard-target="#div-'.sanitize_title($acf_field_group_TITLE).'"> Copy Code </a><br><span style="opacity:0;">--&gt;</span></div>';
+					    echo "<pre>&lt;?php<br></pre>";
+					    echo "<pre class='notebox'>/**";
+					    echo "<br>****************************************************************<br>";
+					    echo "* ACF Field Group Name : ";
+						echo '<a href="'. site_url('/wp-admin/post.php?post=' . $acf_field_group_var_ID . '&action=edit') .'" alt="Go to field group:'.$acf_field_group_TITLE.'">';
+					    echo '',$acf_field_group_TITLE . "<br>";
+					    echo "</a>";
+					    echo "* Made with ACF Theme PHP plugin<br>"; 
+					    echo "****************************************************************<br>*/</pre>"; 
 					endif;
 				
 
 				foreach($acf_field_group_keys_ARRAY as $acf_field_group_keys_EACH){
 
-					$acf_field_group_keys_EACH 	= get_field_object($acf_field_group_keys_EACH);
+					//var_dump($acf_field_field_VAR);
+			        if(!$ACF_PRO_active):
+			        	$acf_field_group_keys_EACH 	= get_field_object($acf_field_group_keys_EACH);
+			        else:
+			        	$acf_field_group_keys_EACH 	= get_field_object($acf_field_group_keys_EACH->post_name);
+			        endif;
+
 					$acf_field_LABEL 			= $acf_field_group_keys_EACH['label'];
 					$acf_field_NAME 			= $acf_field_group_keys_EACH['name'];
-					$acf_field_KEY 			= $acf_field_group_keys_EACH['key'];
+					$acf_field_KEY 				= $acf_field_group_keys_EACH['key'];
 					$acf_field_TYPE 			= $acf_field_group_keys_EACH['type'];
 					$acf_field_INSTRUCTIONS 	= $acf_field_group_keys_EACH['instructions'];
 					?>
@@ -120,8 +198,11 @@ function acf_spv_show_variables(){
 						    	$acf_field_INSTRUCTIONS = preg_replace('!\s+!', ' ', $acf_field_INSTRUCTIONS);
 						    	echo '<pre>';
 
-						    	echo '<a href="'. site_url('/wp-admin/post.php?post=' . $acf_field_group_var_ID . '&action=edit') .'" alt="Go to field group:'.$acf_field_group_TITLE.'">';
-						    	echo '<span class="change-function" data-name1=" $' . $acf_field_NAME . ' &nbsp; &nbsp; = " data-name2=" "> $' . $acf_field_NAME . ' &nbsp; &nbsp; = </span></a>';
+						    	//echo '<a href="'. site_url('/wp-admin/post.php?post=' . $acf_field_group_var_ID . '&action=edit') .'" alt="Go to field group:'.$acf_field_group_TITLE.'">';
+						    	echo '<strong>';
+						    	echo '<span class="change-function" data-name1=" $' . $acf_field_NAME . ' = " data-name2=" "> $' . $acf_field_NAME . ' = </span>';
+						    	echo '</strong>';
+						    	//echo '</a>';
 						    	echo '<span class="change-function" data-name1="get_field" data-name2="the_field">get_field</span>(&#39;<span class="change-keys" data-name1="'.$acf_field_NAME.'" data-name2="'.$acf_field_KEY.'">'.$acf_field_NAME.'</span>&#39;);';
 						    	echo '<span class="hide toggle-type"> // # ' . $acf_field_TYPE . '</span>';
 						    	echo '<span class="hide toggle-instructions" style="font-size:small;"> // #'. htmlspecialchars($acf_field_INSTRUCTIONS) .'</span></pre>';
@@ -151,10 +232,16 @@ function acf_spv_show_variables(){
 
 				foreach($acf_field_group_keys_ARRAY as $acf_field_group_keys_EACH){
 
-					$acf_field_group_keys_EACH 	= get_field_object($acf_field_group_keys_EACH);
+					//var_dump($acf_field_field_VAR);
+			        if(!$ACF_PRO_active):
+			        	$acf_field_group_keys_EACH 	= get_field_object($acf_field_group_keys_EACH);
+			        else:
+			        	$acf_field_group_keys_EACH 	= get_field_object($acf_field_group_keys_EACH->post_name);
+			        endif;
+
 					$acf_field_LABEL 			= $acf_field_group_keys_EACH['label'];
 					$acf_field_NAME 			= $acf_field_group_keys_EACH['name'];
-					$acf_field_KEY 			= $acf_field_group_keys_EACH['key'];
+					$acf_field_KEY 			 	= $acf_field_group_keys_EACH['key'];
 					$acf_field_TYPE 			= $acf_field_group_keys_EACH['type'];
 					$acf_field_INSTRUCTIONS 	= $acf_field_group_keys_EACH['instructions'];
 
@@ -193,8 +280,60 @@ function acf_spv_show_variables(){
 					    </script>
 
 
-	<?php } else {?>
-		<h2>You don't have any fields created yet.</h2>
-		<h3><a href="<?php echo site_url('/wp-admin/post-new.php?post_type=acf'); ?>">Get Started!</a></h3>
+	<?php } else { ?>
+
+		<?php if(!$ACF_PRO_active): ?>
+				<h2>You don't have any fields created yet.</h2>
+				<h3><a href="<?php echo site_url('/wp-admin/post-new.php?post_type=acf'); ?>">Get Started!</a></h3>
+		<?php else: ?>
+				<h2>You don't have any fields created yet for ACF PRO.</h2>
+				<h3><a href="<?php echo site_url('/wp-admin/post-new.php?post_type=acf-field-group'); ?>">Get Started!</a></h3>
+		<?php endif; ?>
+	
 	<?php } // End if ?>
 <?php } //acf_spv_show_variables() ?>
+
+<?php
+	function add_link_to_acf(){
+		if(is_plugin_active('advanced-custom-fields-pro/acf.php') || ( class_exists('acf_pro') )):
+		add_meta_box('ACF-PHP-VARS-ID', 'ACF Theme PHP <span class="description">(Quick View)</span>', 'show_link_to_acfphpvars', 'acf-field-group', 'normal', 'low');
+        else:
+		add_meta_box('ACF-PHP-VARS-ID', 'ACF Theme PHP <span class="description">(Quick View)</span>', 'show_link_to_acfphpvars', 'acf', 'normal', 'low');
+		endif;
+	}
+	add_action('add_meta_boxes', 'add_link_to_acf'); 
+
+	function show_link_to_acfphpvars(){
+
+		?>
+		<style type="text/css">
+/*			.tr-tablehead{display: none;}
+*/
+			table#acf-table{border:none;}
+			pre.notebox{display: none;}
+			.toggle-type-id{display: block;}
+			.toggle-instructions-id{display: nonex;}
+			.toggle-example-id{display: nonex;}
+			.ctc{display: none;}
+			.vartitle{display: none;}
+			.vardesc{display: none;}
+			.toggle-key-id{display: block;}
+			.toggle-function-id{display: block;}
+			.toggle{display: block;}
+			.toggle-container{display: block; margin-top: -18px !important;width: 230px;}
+			/* div#side-sortables { position: fixed;} */
+		</style>
+		<?php
+
+		$postid = get_the_ID();
+		$fgvaru = get_the_title($postid);    
+		$fgvar = sanitize_title(get_the_title($postid));
+
+		if ($fgvar != 'auto-draft'):
+			//echo '<div style="font-size: 14px;margin: 0;line-height: 1.4;" class="acfpv-title-qs">'.$fgvaru.' VARS | Quick Show</div>';
+			acf_spv_show_variables($fgvar);
+			echo '<div style="display:block;text-align:right;border-top:1px solid #ddd;padding: 8px 0 2px 0;" ><a href="admin.php?page=acf_spv_show_variables" class="acf-buttonx largex" id="show-vars-id"/>See All Field Group Vars</a></div>';
+		else:
+			echo 'Please save your field group to see fields';
+		endif;
+	}
